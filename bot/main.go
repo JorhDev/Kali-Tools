4524bfc4d78b16f8081836c78bead64652e3dd15
Jorh <jorhcrypton@gmail.com>
2025-06-29 01:57:42 +0300

    Update main.go

--- bot/main.go
+++ bot/main.go
@@ -1,35 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	//🐤
-	//🔋
-	score := 0
-	score_cell := 1
-
-	for i := 1; i <= 11; i++ {
-
-		fmt.Println("Get Ready")
-		fmt.Println("Счет:", score)
-		fmt.Println("")
-
-		fmt.Println("Вы подлетаете к трубе!", score_cell)
-		fmt.Println("🐤🔋🔋")
-		fmt.Println("")
-
-		fmt.Println("Вы пролетаете через трубу!")
-		fmt.Println("🔋🐤🔋")
-		fmt.Println("")
-
-		fmt.Println("Вы пролетели через трубу!")
-		fmt.Println("🔋🔋🐤")
-		fmt.Println("")
-
-		score++
-		score_cell++
-
-		fmt.Println("Счет:", score)
-		fmt.Println("")
+	mana := 0
+	for i := 0; i < 10000000; i++ {
+		if mana == rand.Intn(100000) {
+			fmt.Println("Выпало число 0")
+			break
+		} else {
+			fmt.Println(i)
+		}
 	}
 }
