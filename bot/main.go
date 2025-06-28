b9dd7ccbed9870d7477093583466b0a5ddb490cd
Jorh <jorhcrypton@gmail.com>
2025-06-28 23:56:55 +0300

    Update main.go

--- bot/main.go
+++ bot/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	score := 5
+
+	if score > 10 {
+		fmt.Println("Ты крассава!")
+	} else {
+		fmt.Println("Ты лох ебанный :(")
+	}
 }
